0e7583d16fd45ea8cca7e365a31a50dd7fd30da4
ma-muktadeer <muktadeer.ma@gmail.com>
2025-07-12 19:02:12 +0600

    theme done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('material-theme');
+  currentTheme = signal<'lignt-theme' | 'dark-theme'>('lignt-theme');
+
+  toggleTheme() {
+    console.log('theme color', this.currentTheme());
+
+    this.currentTheme.update(theme => theme === 'lignt-theme' ? 'dark-theme' : 'lignt-theme');
+  }
 }
